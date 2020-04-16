570ecf0f84ecd76f90348441bcd68286ba224cc6
Roniel <roniel@Roniel-MacBook-Pro-12.local>
2020-04-15 22:42:04 -0400

    first commit

---
+++ src/cli.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const pkg = require('../package.json');
+
+const cli = (args) => {
+    program.version(pkg.version);
+
+    program.command('create-project <name>')
+        .alias('cp')
+        .description('Create a project.')
+        .option('--withRedux <withRedux>', 'The project configuration with Redux', true)
+        .action((ap, aa) => console.log(ap, aa));
+
+    program.command('component <name>')
+        .alias('c')
+        .description('Generate a component.')
+        .action((ap, aa) => console.log(ap, aa));
+
+    program.command('page <name>')
+        .alias('p')
+        .description('Generate a page.');
+
+    program.command('redux-config')
+        .alias('rc')
+        .description('Setup the project with Redux.');
+
+    program.parse(args);
+};
+
+cli(process.argv);
